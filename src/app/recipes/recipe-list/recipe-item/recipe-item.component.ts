c44f92c610e9f887a87f299fd3f928b906b8bf89
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-12 01:41:40 +0500

    learning

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent {
+
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://get.pxhere.com/photo/food-cuisine-ingredient-dish-recipe-tableware-garnish-culinary-art-finger-food-fast-food-dessert-plate-snack-baked-goods-meal-side-dish-dairy-condiment-comfort-food-junk-food-breakfast-delicacy-Chiboust-cream-american-food-sandwich-hot-dog-Breakfast-roll-kitchen-utensil-cake-decorating-staple-food-la-carte-food-produce-buttercream-cooking-icing-cream-italian-food-bavarian-cream-brunch-al-dente-mediterranean-food-1636540.jpg")
+  ];
 }
