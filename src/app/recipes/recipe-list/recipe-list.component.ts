75a71f4cde8aa964598bdb065b7f08063e26c005
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-23 17:44:40 +0500

    learning

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe("A Test Recipe", "This is simply a test", "https://get.pxhere.com/photo/food-cuisine-ingredient-dish-recipe-tableware-garnish-culinary-art-finger-food-fast-food-dessert-plate-snack-baked-goods-meal-side-dish-dairy-condiment-comfort-food-junk-food-breakfast-delicacy-Chiboust-cream-american-food-sandwich-hot-dog-Breakfast-roll-kitchen-utensil-cake-decorating-staple-food-la-carte-food-produce-buttercream-cooking-icing-cream-italian-food-bavarian-cream-brunch-al-dente-mediterranean-food-1636540.jpg"),
-    new Recipe("A Test Recipe", "This is simply a test", "https://get.pxhere.com/photo/food-cuisine-ingredient-dish-recipe-tableware-garnish-culinary-art-finger-food-fast-food-dessert-plate-snack-baked-goods-meal-side-dish-dairy-condiment-comfort-food-junk-food-breakfast-delicacy-Chiboust-cream-american-food-sandwich-hot-dog-Breakfast-roll-kitchen-utensil-cake-decorating-staple-food-la-carte-food-produce-buttercream-cooking-icing-cream-italian-food-bavarian-cream-brunch-al-dente-mediterranean-food-1636540.jpg"),
+    new Recipe("Another Test Recipe", "This is simply a test", "https://get.pxhere.com/photo/food-cuisine-ingredient-dish-recipe-tableware-garnish-culinary-art-finger-food-fast-food-dessert-plate-snack-baked-goods-meal-side-dish-dairy-condiment-comfort-food-junk-food-breakfast-delicacy-Chiboust-cream-american-food-sandwich-hot-dog-Breakfast-roll-kitchen-utensil-cake-decorating-staple-food-la-carte-food-produce-buttercream-cooking-icing-cream-italian-food-bavarian-cream-brunch-al-dente-mediterranean-food-1636540.jpg"),
   ];
 
   onRecipeSelected(recipe: Recipe) {
